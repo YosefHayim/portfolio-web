14881bf9d11149481fc99b8da2f1773408334c72
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-16 19:58:43 +0300

    Add routing and components for homepage, about, projects, tech stack, certifications, and 404 page; remove unused JetBrains Mono font files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { Route } from "react-router";
+import Homepage from "./Components/Homepage/Homepage";
+import About from "./Components/About/About";
+import Certifications from "./Components/Certifications/Certifications";
+import TechStack from "./Components/TechStack/TechStack";
+import Projects from "./Components/Projects/Projects";
+import NotFound404 from "./Components/NotFound404/NotFound404";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/techStack" element={<TechStack />} />
+      <Route path="/certifications" element={<Certifications />} />
+      <Route path="/404" element={<NotFound404 />} />
+    </div>
+  );
 };
 
 export default App;

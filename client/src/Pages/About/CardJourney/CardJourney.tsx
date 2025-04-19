200714ee2f47d68a7c8a752679bfdcee718d88aa
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-19 18:53:22 +0300

    feat: update CardJourney component usage in MyJourney for consistent hover color styling and improved structure

--- client/src/Pages/About/CardJourney/CardJourney.tsx
+++ client/src/Pages/About/CardJourney/CardJourney.tsx
@@ -14,7 +14,7 @@ const CardJourney: React.FC<{
         <div className="w-full flex-col items-center justify-start gap-2 p-5">
           <h3 className="w-full text-xl">{title}</h3>
           <div
-            className={`absolute top-[-5%] left-[-4.5%] rounded-full bg-gray-700 p-2 delay-150 duration-300 ease-in-out group-hover:-translate-y-2 group-hover:text-[${colorIconHover}] group-hover:transition hover:scale-110`}
+            className={`absolute top-[-5%] left-[-4.5%] rounded-full bg-gray-700 p-2 delay-150 duration-300 ease-in-out group-hover:-translate-y-2 ${colorIconHover} group-hover:transition hover:scale-110`}
           >
             {icon}
             {children}
--- client/src/Pages/About/MyJourney/MyJourney.tsx
+++ client/src/Pages/About/MyJourney/MyJourney.tsx
@@ -1,26 +1,31 @@
 import { IoMdTrophy } from "react-icons/io";
 import CardJourney from "../CardJourney/CardJourney";
+import { FaUserTie } from "react-icons/fa";
 
 const MyJourney = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-18">
       <h2 className="text-2xl">My journey</h2>
       <CardJourney
-        icon={<IoMdTrophy size={20} />}
-        colorIconHover={`#fdc700`}
-        years={`2024 - 2025`}
-        title={`Fullstack Bootcamp - IITC College`}
-        text={`Completed an intensive 796-hour bootcamp program Full-Stack web development with HTML, CSS, JavaScript, React,
-        Node.js, Express, and databases such as MongoDB and NoSQL`}
-      ></CardJourney>
+        icon={<FaUserTie size={20} />}
+        colorIconHover={`group-hover:text-[#05df72]`}
+        years={`2025 - Active`}
+        title={`Wotch Health Startup - Internship`}
+        text={`Built a real-time debugging tool, improved E2E tests with Playwright, and integrated external data using EJS. Worked full stack in a fast-paced healthcare startup with hands-on SQL, cloud, and production experience.`}
+      />
       <CardJourney
         icon={<IoMdTrophy size={20} />}
-        colorIconHover={`#fdc700`}
+        colorIconHover={`group-hover:text-[#fdc700]`}
+        years={`2024 - 2025`}
+        title={`Fullstack Bootcamp - IITC College`}
+        text={`Completed an intensive 796-hour bootcamp program Full-Stack web development with HTML, CSS, JavaScript, React,Node.js, Express, and databases such as MongoDB and NoSQL`}
+      />
+      <CardJourney
+        icon={<IoMdTrophy size={20} />}
+        colorIconHover={`group-hover:text-[#fdc700]`}
         years={`2018 - 2021`}
         title={`Infantry Commander - IDF - 931st Battalion - Negevist`}
-        text={`Served as a combat Negevist and completed training as an outstanding soldier in the Nahal
-      Brigade recruitment cycle. Recognized for excellence in training and leadership during the IDF
-      Commanders’ Course, where I developed strong leadership and problem-solving skills`}
+        text={`I served in the IDF as a combat soldier (Negevist) in the Nahal Brigade. I completed my training as one of the top soldiers and was awarded excellence twice – once during basic training and again in the Commanders’ Course. I led soldiers in the field and gained strong leadership and problem-solving skills through real-life challenges.`}
       >
         <p className={`#fdc700 absolute top-3 left-7 hidden group-hover:block`}>2x</p>
       </CardJourney>

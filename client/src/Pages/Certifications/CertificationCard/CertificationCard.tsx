32a1d0cca288adff503ccee9414f420200e94ebf
yosefhayim <yosefisabag+03@gmail.com>
2025-04-21 21:13:04 +0300

    feat: implement CertificationCard component with props for icon, title, year, and link; update CertificationCards to display multiple certificates

--- client/src/Pages/Certifications/CertificationCard/CertificationCard.tsx
+++ client/src/Pages/Certifications/CertificationCard/CertificationCard.tsx
@@ -1,5 +1,24 @@
-const CertificationCard = () => {
-  return <div></div>;
+import { ReactNode } from "react";
+import { Link } from "react-router";
+
+const CertificationCard: React.FC<{
+  icon: ReactNode;
+  certificateTitle: string;
+  yearEarnedCertificate: string;
+  certificateLink: string;
+}> = ({ icon, yearEarnedCertificate, certificateTitle, certificateLink }) => {
+  return (
+    <div className="flex w-full flex-col items-start justify-start rounded-sm bg-gray-800 p-2 text-sm">
+      <div className="flex w-full items-start justify-center">
+        <p>{icon}</p>
+        <p>{certificateTitle}</p>
+        <p>{yearEarnedCertificate}</p>
+      </div>
+      <Link to={certificateLink} className="flex items-center">
+        View Certificate
+      </Link>
+    </div>
+  );
 };
 
 export default CertificationCard;
--- client/src/Pages/Certifications/CertificationsCards/CertificationCards.tsx
+++ client/src/Pages/Certifications/CertificationsCards/CertificationCards.tsx
@@ -1,9 +1,27 @@
+import { FaNodeJs, FaPython } from "react-icons/fa";
 import CertificationCard from "../CertificationCard/CertificationCard";
 
 const CertificationCards = () => {
   return (
-    <div>
-      <CertificationCard />
+    <div className="flex w-full flex-col items-start justify-start gap-2">
+      <CertificationCard
+        icon={<FaPython size={30} />}
+        certificateTitle={"100 Days of Code: The Complete Python Pro Bootcamp"}
+        yearEarnedCertificate={"Oct 2024"}
+        cretificateLink={"Nov 2024"}
+      ></CertificationCard>
+      <CertificationCard
+        icon={<FaNodeJs size={30} />}
+        certificateTitle={"Node.js, Express, MongoDB & More: The Complete Bootcamp"}
+        yearEarnedCertificate={"Feb 2025"}
+        cretificateLink={""}
+      ></CertificationCard>
+      <CertificationCard
+        icon={<FaPython size={30} />}
+        certificateTitle={"The Complete Full-Stack Web Development Bootcamp"}
+        yearEarnedCertificate={""}
+        cretificateLink={""}
+      ></CertificationCard>
     </div>
   );
 };

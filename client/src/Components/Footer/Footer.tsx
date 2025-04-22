3a7cdca1fe0bcc24132eacc07875a323322728ae
yosefhayim <yosefisabag+03@gmail.com>
2025-04-22 10:53:34 +0300

    navigation

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <p className="text-gray-400">Trying to get better everyday.</p>
       </section>
       <section className="flex w-full flex-col items-start justify-start gap-2">
-        <h2>Navigation</h2>
+        <h2>Navigation menu</h2>
         <nav className="flex w-full flex-col items-center gap-2">
           <NavigationButton to="/" pageName="Home" />
           <NavigationButton to="/about" pageName="About" />

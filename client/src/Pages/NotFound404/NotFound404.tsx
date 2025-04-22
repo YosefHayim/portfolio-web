e15dcdf3b07a01a03b22ef8b61dcca0566a6d480
yosefhayim <yosefisabag+03@gmail.com>
2025-04-22 11:42:09 +0300

    feat: enhance NotFound404 component with navigation redirect and error message display

--- client/src/Pages/NotFound404/NotFound404.tsx
+++ client/src/Pages/NotFound404/NotFound404.tsx
@@ -1,10 +1,27 @@
 import { useEffect } from "react";
+import { GoDotFill } from "react-icons/go";
+import { useNavigate } from "react-router";
 
 const NotFound404 = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     document.title = "404";
+
+    const timeout = setTimeout(() => {
+      navigate("/");
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
   }, []);
-  return <div></div>;
+
+  return (
+    <div className="p-2">
+      <GoDotFill size={28} className="text-red-400" />
+      <p>This is 404 error page.</p>
+    </div>
+  );
 };
 
 export default NotFound404;

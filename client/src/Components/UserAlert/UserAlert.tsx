a956e230b9666ca2edaf334a65694477a4396aa5
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-22 12:55:34 +0300

    feat: add UserAlert component for alert notifications and integrate it into WhoAmI component

---
+++ client/src/Components/UserAlert/UserAlert.tsx
@@ -0,0 +1,15 @@
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/Components/ui/alert";
+
+const UserAlert: React.FC<{ alertTitle: string; alertDescription: string }> = ({ alertTitle, alertDescription }) => {
+  return (
+    <Alert className="border-none bg-gray-700 text-white">
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>{alertTitle}</AlertTitle>
+      <AlertDescription className="text-gray-400">{alertDescription}</AlertDescription>
+    </Alert>
+  );
+};
+
+export default UserAlert;
--- client/src/Pages/Homepage/WhoAmI/WhoAmI.tsx
+++ client/src/Pages/Homepage/WhoAmI/WhoAmI.tsx
@@ -1,3 +1,5 @@
+import UserAlert from "@/Components/UserAlert/UserAlert";
+import { useEffect, useState } from "react";
 import { FaSort } from "react-icons/fa";
 import { FiMinus } from "react-icons/fi";
 import { GoDotFill } from "react-icons/go";
@@ -5,72 +7,94 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
 
 const WhoAmI = () => {
-  return (
-    <div className="group relative flex w-full flex-col items-start justify-start rounded-xl bg-gray-800">
-      <div className="flex items-center justify-start p-1">
-        <div className="relative cursor-pointer">
-          <GoDotFill size={28} className="text-red-400" />
-          <IoCloseOutline className="translate-x-[5.5px] translate-y-[-22px] text-black" />
-        </div>
-        <div className="relative">
-          <GoDotFill size={28} className="text-yellow-400" />
-          <FiMinus className="translate-x-[5.5px] translate-y-[-22px] text-black" />
-        </div>
+  const [alert, setAlert] = useState(false);
 
-        <div className="relative">
-          <GoDotFill size={28} className="text-green-400" />
-          <FaSort className="translate-x-[5.5px] translate-y-[-22px] rotate-120 text-black" />
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [alert]);
+
+  return (
+    <div>
+      {alert && (
+        <div className="pb-3">
+          <UserAlert
+            alertTitle="Oops... Not Quite!"
+            alertDescription="That felt like a Mac close button, huh? Just vibes nothing's actually closing. Still love Apple tho!"
+          />
         </div>
-      </div>
-      <ul>
-        <div className="flex w-full flex-col items-start">
+      )}
+
+      <div className="group relative flex w-full flex-col items-start justify-start rounded-xl bg-gray-800">
+        <div className="flex items-center justify-start p-1">
+          <div className="relative cursor-pointer" onClick={() => setAlert((prev) => !prev)}>
+            <GoDotFill size={28} className="text-red-400" />
+            <IoCloseOutline className="translate-x-[5.5px] translate-y-[-22px] text-black" />
+          </div>
+          <div className="relative">
+            <GoDotFill size={28} className="text-yellow-400" />
+            <FiMinus className="translate-x-[5.5px] translate-y-[-22px] text-black" />
+          </div>
+          <div className="relative">
+            <GoDotFill size={28} className="text-green-400" />
+            <FaSort className="translate-x-[5.5px] translate-y-[-22px] rotate-120 text-black" />
+          </div>
+        </div>
+        <ul>
+          <div className="flex w-full flex-col items-start">
+            <li className="group relative w-full items-center justify-start gap-1 p-2">
+              <div className="flex items-center">
+                <IoIosArrowForward />
+                <h3>WhoiAm?</h3>
+              </div>
+              <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Full-stack developer blending clean code with creative problem-solving.
+              </p>
+            </li>
+          </div>
           <li className="group relative w-full items-center justify-start gap-1 p-2">
-            <div className="flex items-center">
+            <div className="flex w-full items-center">
               <IoIosArrowForward />
-              <h3>WhoiAm?</h3>
+              <h3>Stack</h3>
             </div>
             <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-              Full-stack developer blending clean code with creative problem-solving.
+              React, Node.js, TypeScript, PostgreSQL, Playwright, Kafka.
             </p>
           </li>
-        </div>
-        <li className="group relative w-full items-center justify-start gap-1 p-2">
-          <div className="flex w-full items-center">
-            <IoIosArrowForward />
-            <h3>Stack</h3>
-          </div>
-          <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            React, Node.js, TypeScript, PostgreSQL, Playwright, Kafka.
-          </p>
-        </li>
-        <li className="group relative w-full items-center justify-start gap-1 p-2">
-          <div className="flex w-full items-center">
-            <IoIosArrowForward />
-            <h3>Focus</h3>
-          </div>
-          <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            Building real-time apps, scalable APIs, and testable systems.
-          </p>
-        </li>
-        <li className="group relative w-full items-center justify-start gap-1 p-2">
-          <div className="flex w-full items-center">
-            <IoIosArrowForward />
-            <h3>Passion</h3>
-          </div>
-          <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            Automating workflows, shipping fast, and learning endlessly.
-          </p>
-        </li>
-        <li className="group relative w-full items-center justify-start gap-1 p-2">
-          <div className="flex w-full items-center">
-            <IoIosArrowForward />
-            <h3>Motto</h3>
-          </div>
-          <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            Trying to get better <span className="font-bold group-hover:text-green-400">everyday.</span>
-          </p>
-        </li>
-      </ul>
+          <li className="group relative w-full items-center justify-start gap-1 p-2">
+            <div className="flex w-full items-center">
+              <IoIosArrowForward />
+              <h3>Focus</h3>
+            </div>
+            <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Building real-time apps, scalable APIs, and testable systems.
+            </p>
+          </li>
+          <li className="group relative w-full items-center justify-start gap-1 p-2">
+            <div className="flex w-full items-center">
+              <IoIosArrowForward />
+              <h3>Passion</h3>
+            </div>
+            <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Automating workflows, shipping fast, and learning endlessly.
+            </p>
+          </li>
+          <li className="group relative w-full items-center justify-start gap-1 p-2">
+            <div className="flex w-full items-center">
+              <IoIosArrowForward />
+              <h3>Motto</h3>
+            </div>
+            <p className="text-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Trying to get better <span className="font-bold group-hover:text-green-400">everyday.</span>
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };

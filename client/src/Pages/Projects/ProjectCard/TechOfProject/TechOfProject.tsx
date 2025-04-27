69e608417f55749c74511b3586c4cd278dfb59f3
yosefhayim <yosefisabag+03@gmail.com>
2025-04-27 13:35:25 +0300

    feat: update font usage in TechOfProject component

--- client/src/Pages/Projects/ProjectCard/TechOfProject/TechOfProject.tsx
+++ client/src/Pages/Projects/ProjectCard/TechOfProject/TechOfProject.tsx
@@ -2,7 +2,7 @@ import TechnologyBubble from "../TechBubble/TechBubble";
 
 const TechOfProject: React.FC<{ techStackForProject: string[] }> = ({ techStackForProject }) => {
   return (
-    <div className="flex w-full flex-wrap items-center justify-start gap-2">
+    <div className="flex w-full flex-wrap items-center justify-start gap-2 font-(family-name:--code-font)">
       {techStackForProject.map((techName) => (
         <TechnologyBubble key={techName} techName={techName} />
       ))}

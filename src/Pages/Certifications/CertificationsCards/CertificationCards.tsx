64114c85edade8e8349c8d98a8a4d34ccb308140
yosefhayim <yosefisabag+03@gmail.com>
2025-05-13 14:20:59 +0300

    f

--- src/Pages/Certifications/CertificationsCards/CertificationCards.tsx
+++ src/Pages/Certifications/CertificationsCards/CertificationCards.tsx
@@ -19,19 +19,19 @@ const CertificationCards = () => {
         certificateLink={"https://www.udemy.com/certificate/UC-830343b5-2bb6-44ae-baf3-af70748ea84c/"}
       ></CertificationCard>
       <CertificationCard
-        icon={<FaPython size={30} />}
-        certificateTitle={"100 Days of Code: The Complete Python Pro Bootcamp"}
-        yearEarnedCertificate={"Oct 2024"}
-        certificateLink={
-          "https://www.udemy.com/certificate/UC-65f92c9d-6851-4700-9ced-8cfa8d192b41/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com"
-        }
-      ></CertificationCard>
-      <CertificationCard
         icon={<IoLogoJavascript size={30} />}
         certificateTitle={"The Complete Full-Stack Web Development Bootcamp"}
         yearEarnedCertificate={"Nov 2024"}
         certificateLink={
           "https://www.udemy.com/certificate/UC-2bc9ed93-536f-4dd1-9aeb-45821941d8bc/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com"
+        }
+      ></CertificationCard>
+      <CertificationCard
+        icon={<FaPython size={30} />}
+        certificateTitle={"100 Days of Code: The Complete Python Pro Bootcamp"}
+        yearEarnedCertificate={"Oct 2024"}
+        certificateLink={
+          "https://www.udemy.com/certificate/UC-65f92c9d-6851-4700-9ced-8cfa8d192b41/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com"
         }
       ></CertificationCard>
     </div>

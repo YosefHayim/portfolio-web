3ff55065dd88b18e3e7d3b31c79a8c59229fcde1
YosefHayim <yosefisabag+03@gmail.com>
2026-04-29 03:59:04 +0300

    preserve https nginx config during ec2 sync

--- infra/aws/bootstrap-instance.sh
+++ infra/aws/bootstrap-instance.sh
@@ -130,6 +130,63 @@ sudo cp "$ROOT_DIR/infra/aws/nginx.portfolio.conf" /etc/nginx/conf.d/portfolio.c
 sudo sed -i "s/__DOMAIN_ROOT__/${DOMAIN_ROOT:-yosefhayimsabag.com}/g" /etc/nginx/conf.d/portfolio.conf
 sudo sed -i "s/__API_SUBDOMAIN__/${API_SUBDOMAIN:-api.yosefhayimsabag.com}/g" /etc/nginx/conf.d/portfolio.conf
 
+CERT_NAME="${CERT_NAME:-${DOMAIN_ROOT:-yosefhayimsabag.com}}"
+CERT_DIR="/etc/letsencrypt/live/$CERT_NAME"
+
+if [[ -f "$CERT_DIR/fullchain.pem" && -f "$CERT_DIR/privkey.pem" ]]; then
+  cat <<NGINX >/tmp/portfolio.conf
+server {
+    listen 80;
+    server_name ${DOMAIN_ROOT:-yosefhayimsabag.com} www.${DOMAIN_ROOT:-yosefhayimsabag.com} ${API_SUBDOMAIN:-api.yosefhayimsabag.com};
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    http2 on;
+    server_name ${DOMAIN_ROOT:-yosefhayimsabag.com} www.${DOMAIN_ROOT:-yosefhayimsabag.com};
+
+    ssl_certificate $CERT_DIR/fullchain.pem;
+    ssl_certificate_key $CERT_DIR/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    location / {
+        proxy_pass http://127.0.0.1:8080;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+
+server {
+    listen 443 ssl;
+    http2 on;
+    server_name ${API_SUBDOMAIN:-api.yosefhayimsabag.com};
+
+    ssl_certificate $CERT_DIR/fullchain.pem;
+    ssl_certificate_key $CERT_DIR/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    location / {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_http_version 1.1;
+        proxy_buffering off;
+        proxy_cache off;
+        proxy_read_timeout 300s;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+NGINX
+  sudo mv /tmp/portfolio.conf /etc/nginx/conf.d/portfolio.conf
+fi
+
 if [[ -f /etc/nginx/conf.d/default.conf ]]; then
   sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.disabled
 fi
